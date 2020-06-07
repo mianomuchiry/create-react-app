047c4c6fe54a96bce8f00890d93bda9604deb3d1
Miano Muchiri <mianomuchiry@gmail.com>
2020-06-07 19:36:29 +0300

    Card and card list component Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -7,28 +7,20 @@ class App extends Component {
 		super();
 
 		this.state = {
-			monsters: [
-				{
-					name: 'Frankenstein',
-					id: 'a1'
-				},
-				{
-					name: 'Dracula',
-					id: 'a2'
-				},
-				{
-					name: 'Zombie',
-					id: 'a3'
-				}
-			]
+			monsters: []
 		};
 	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((users) => this.setState({ monsters: users }));
+	}
+
 	render() {
 		return (
 			<div className='App'>
-				{this.state.monsters.map((monster) => (
-					<h1 key={monster.id}>{monster.name}</h1>
-				))}
+				<CardList monsters={this.state.monsters}></CardList>
 			</div>
 		);
 	}
