1f74470ca8fd188aa35ce455bad5f371c7ee51a4
Miano Muchiri <mianomuchiry@gmail.com>
2020-06-07 21:59:29 +0300

    Updates

---
+++ precache-manifest.6b42dafc6bac59a8880115606fe05f9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e76ceb79a555efcb99c6ec55a098ea4",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "03dc1eea6b425d48cca7",
+    "url": "/create-react-app/static/css/main.c12f0fe0.chunk.css"
+  },
+  {
+    "revision": "fe3a224c0d7233fc34a9",
+    "url": "/create-react-app/static/js/2.4ece4643.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react-app/static/js/2.4ece4643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03dc1eea6b425d48cca7",
+    "url": "/create-react-app/static/js/main.0dcf544b.chunk.js"
+  },
+  {
+    "revision": "7571d4788ee5b773f815",
+    "url": "/create-react-app/static/js/runtime-main.4720e1ac.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mianomuchiry/create-react-app/precache-manifest.c0c2269923a3f145232a30cafabf78bc.js"
+  "/create-react-app/precache-manifest.6b42dafc6bac59a8880115606fe05f9e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mianomuchiry/create-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/create-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
