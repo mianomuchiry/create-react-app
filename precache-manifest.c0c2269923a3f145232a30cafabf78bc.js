959ed80a7134901b4e45b664cee57e145ec0fe5a
Miano Muchiri <mianomuchiry@gmail.com>
2020-06-07 21:52:53 +0300

    Updates

---
+++ precache-manifest.c0c2269923a3f145232a30cafabf78bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b544ade0350ef680832dc26001920cc",
+    "url": "/mianomuchiry/create-react-app/index.html"
+  },
+  {
+    "revision": "e86d8a5d6dc64ba501e0",
+    "url": "/mianomuchiry/create-react-app/static/css/main.c12f0fe0.chunk.css"
+  },
+  {
+    "revision": "fe3a224c0d7233fc34a9",
+    "url": "/mianomuchiry/create-react-app/static/js/2.4ece4643.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mianomuchiry/create-react-app/static/js/2.4ece4643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86d8a5d6dc64ba501e0",
+    "url": "/mianomuchiry/create-react-app/static/js/main.5701a02f.chunk.js"
+  },
+  {
+    "revision": "cd7cea914588c9fbeeb4",
+    "url": "/mianomuchiry/create-react-app/static/js/runtime-main.1f63cd70.js"
+  }
+]);
